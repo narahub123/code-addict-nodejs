b7c6d21c5609895cff0121fb1db7df6cf3decc03
nara <parknara0213@gmail.com>
2024-02-04 13:58:26 +0900

    240204

--- 02-express-tutorial/01-http-basic.js
+++ 02-express-tutorial/01-http-basic.js
@@ -1,15 +1,17 @@
-// access to http module 
+// access to http module
 const http = require("http");
 
-// create a server 
-// the callback which is gonna be invoked every time 
-// user hit the home page 
+// create a server
+// the callback which is gonna be invoked every time
+// user hit the home page
 const server = http.createServer((req, res) => {
   console.log("user hit the server");
-  // in every response we should always have end 
-  // which is gonna signal that the communication is over 
+  // in every response we should always have end
+  // which is gonna signal that the communication is over
   res.end("home page");
 });
 
-// set up a port 
+// set up a port
 server.listen(5000);
+
+// thisis it
