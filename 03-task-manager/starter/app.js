31ab4714b12aac9660eade9738711912af65008a
nara <parknara0213@gmail.com>
2024-02-05 14:37:12 +0900

    240205

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
+
+// middleware
+
+app.use(express.json());
 
 // routes
 app.get("/hello", (req, res) => {
   res.send("Task Manager App");
 });
+
+app.use("/api/v1/tasks", tasks);
 
 // app.get('api/v1/tasks')          - get all the task
 // app.post('api/v1/tasks')         - create a new task
