e217895a88a9eb5de38986c4641d7979f3d66232
nara <parknara0213@gmail.com>
2024-02-05 19:16:05 +0900

    240205

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,16 +1,12 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://john:1234@nodeexpressproject.viwrjth.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority";
-
-// connect to db
-mongoose
-  .connect(connectionString, {
-    // remove deprecation warning : version 6 above don't need this
+const connectDB = (url) => {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
     useUnifiedTopology: true,
-  })
-  .then(() => console.log("CONNTECTED TO THE DB.."))
-  .catch((err) => console.log(err));
+  });
+};
+
+module.exports = connectDB;
