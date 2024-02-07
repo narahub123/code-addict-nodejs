2184ce9cc35c243851c6eef0e9d9ee1772b3853a
nara <parknara0213@gmail.com>
2024-02-07 22:28:24 +0900

    240207

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,42 @@
-console.log('04 Store API')
+// set up for connecting database
+require("dotenv").config();
+
+// asynce errors
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+// middleware
+const NotFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+// routes
+// home route
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+app.use("/api/v1/products", productsRouter);
+
+// products route
+
+app.use(NotFoundMiddleware);
+app.use(errorMiddleware);
+
+// connection to db
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    // connectDB
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening port ${port}...`));
+  } catch (error) {}
+};
+
+start();
---
+++ 04-store-api/starter/models/product.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "product name must be provided"],
+  },
+  price: {
+    type: number,
+    require: [true, "product price must be provided"],
+  },
+  feature: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: number,
+    default: 4.5,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: "{VALUES} is not supported",
+    },
+    // enum: ["ikea", "liddy", "caressa", "marcos"],
+  },
+}); // productSchema ends
+
+module.exports = mongoose.model("Product", productSchema);
