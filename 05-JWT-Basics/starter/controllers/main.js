4337f68ddd76644afd1f3d10dcb32953e2b50254
nara <parknara0213@gmail.com>
2024-02-09 21:29:17 +0900

    240209

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -23,11 +23,32 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const luckyNumber = Math.floor(Math.random() * 100);
-  res.status(200).json({
-    msg: `Hello, John Doe`,
-    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-  });
+  // console.log(req.headers);
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+
+  // get the token
+  const token = authHeader.split(" ")[1];
+
+  // verification
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    //console.log(decoded);
+
+    const luckyNumber = Math.floor(Math.random() * 100);
+
+    res.status(200).json({
+      msg: `Hello, ${decoded.username}`,
+      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+    });
+  } catch (error) {
+    throw new CustomAPIError("Not authrozied to access this route", 401);
+  }
+
+  //console.log(token);
 };
 
 module.exports = {
