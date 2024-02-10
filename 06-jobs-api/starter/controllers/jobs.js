19e231e0e57de796282bdb99aa8d0399d0832043
nara <parknara0213@gmail.com>
2024-02-10 23:30:07 +0900

    240210 get job

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -10,7 +10,21 @@ const getAlljobs = async (req, res) => {
 };
 
 const getJob = async (req, res) => {
-  res.send("get job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId,
+  });
+
+  if (!job) {
+    throw new NotFoundErrors(`No job with id ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
