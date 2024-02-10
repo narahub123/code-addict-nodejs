1fed7d9fea59d671362eb8cca504ff1924f5f9e5
nara <parknara0213@gmail.com>
2024-02-10 23:20:23 +0900

    240210 get all jobs

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -2,8 +2,11 @@ const Job = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundErrors } = require("../errors");
 
+// looking for jobs associated with the user
 const getAlljobs = async (req, res) => {
-  res.send("get all jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createAt");
+
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJob = async (req, res) => {
