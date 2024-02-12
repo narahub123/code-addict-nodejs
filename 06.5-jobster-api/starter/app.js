7e620a6e1186cd61dc35d5d1a55504aadb737819
nara <parknara0213@gmail.com>
2024-02-12 19:52:55 +0900

    240212 limit accessibility

--- 06.5-jobster-api/starter/app.js
+++ 06.5-jobster-api/starter/app.js
@@ -19,6 +19,9 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+// set up hosting
+app.set("trust proxy", 1);
+
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(express.json());
 app.use(helmet());
